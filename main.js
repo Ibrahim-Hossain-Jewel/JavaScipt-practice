8f721dc2818e608410e6fca38b300d9491ae577f
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-10 10:37:25 +0600

    array reduce() method i did added number

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var array =["jewel","saju","Raju vaia","Fahim",7,8,9];
-var array2 = array.map((Element)=>{
-    console.log(Element);
+var array = [1,2,3,4,5,6,7];
+var counter = 0;
+var redu = array.reduce((x,y)=>{
+    counter++;
+    return x+y;
 });
+console.log(redu);
+console.log(counter);
