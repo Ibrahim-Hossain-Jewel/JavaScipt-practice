6830f61631ecc5f9e7d9460ef2cec84d221a8432
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-11 05:15:16 +0600

    setInterval() method and clearInterval() method

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 //learning the setinterval() and settimeout();
 console.log('start setinterval');
-setInterval(()=>{
-    document.getElementsByClassName('other')[0].innerHTML="JavaScript is a programming language";
-},2000);
+var x =0;
+var id = setInterval(()=>{ //setinterval() use to looping a object; the setInterval return a id that's by you can control your flow
+    document.getElementsByClassName('other')[0].innerHTML=x++;
+    if(x==30){
+        clearInterval(id);//clearInterval for last destination;
+    }
+
+},500);
 console.log('end setinterval');
