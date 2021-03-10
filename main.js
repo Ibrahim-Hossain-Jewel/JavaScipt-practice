8574d96c065f78aedd033c4cf3853d697eaadff5
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-10 13:03:28 +0600

    the find() method of array

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var food = ['meet','apple','potato'];
-console.log(food.indexOf('meet')>-1); //if the object is present then return the 0 or absent then return -1;
-//along with the >-1 help to find-out how? meet is present then return true otherwise false;
-//here true value has high precedence that's why return true otherwise false;
+//array object overhere;
+var human = [
+    {name:"jewel"
+    ,age : 23,
+    weight: 77
+},
+    {
+        name:"tosiba akter",
+        age : 13,
+        weight: 50
+    }
+];
+//now find the tosiba akter object
+const myname = human.find(qualigName =>{
+    return qualigName.age==23;
+});
+console.log(myname);
