29e1b80d45a72a3137e0e2a776099ec9cceef3f5
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-10 08:20:54 +0600

    array element with foreach method

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var array =[1,2,3,5,7,8,9];
-var array2 = array.filter(Element=>{
-    if(Element>2) return true;
-    return false;
+array.forEach(Element =>{
+    console.log(Element);
 });
-console.log(array2);
