58d61a86841f567683ed57b6d5c044234ff61ccc
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-10 21:49:53 +0600

    setinterval method practice

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-//array object overhere;
-var human = [
-    {name:"jewel"
-    ,age : 23,
-    weight: 77
-},
-    {
-        name:"tosiba akter",
-        age : 13,
-        weight: 50
-    }
-];
-//now find the tosiba akter object
-const myname = human.find(qualigName =>{
-    return qualigName.age==23;
-});
-console.log(myname);
+//learning the setinterval() and settimeout();
+console.log('start setinterval');
+setInterval(()=>{
+    document.getElementsByClassName('other')[0].innerHTML="JavaScript is a programming language";
+},2000);
+console.log('end setinterval');
