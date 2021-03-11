4e14d729af03c6b0970ad70ecbf752667bffd56a
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-11 08:07:27 +0600

    setTimeout() method
    the setTimeout method dosen't increment value

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-//learning the setinterval() and settimeout();
-console.log('start setinterval');
-var x =0;
-var id = setInterval(()=>{ //setinterval() use to looping a object; the setInterval return a id that's by you can control your flow
-    document.getElementsByClassName('other')[0].innerHTML=x++;
-    if(x==30){
-        clearInterval(id);//clearInterval for last destination;
-    }
-
-},500);
-console.log('end setinterval');
+console.log("starting setTimeout() function")
+setTimeout(()=>{
+    let x=2;
+    let dom = document.getElementsByClassName("other")[0];
+    dom.innerHTML = x;
+},1000);
+//the setTimeout() method dosen't increment the value
