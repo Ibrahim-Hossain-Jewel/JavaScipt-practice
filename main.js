3fd1494dc6eb5e0bf695d5e1f9e3d3f8562031a2
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-12 09:28:00 +0600

    Converting money bangladeshi to USD $
    This is a just mini practice application

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var btn = document.querySelector('button');
 var cost = document.querySelector('input');
-var output = document.querySelector('h1');
+var output = document.querySelector('h2');
 //these are has global access right now;
 //but the the var variable declaration is function scoped;
 output.innerText='The maximum fractional part is two';
 btn.addEventListener('click',()=>{
     let holder = (cost.value);
     //here let has bracket scope;
-    output.innerHTML = holder;
+    output.innerHTML = `<h1>The provided Bangladeshi money is ${holder} and in USD ${((1/80)*holder).toFixed(2)} $ Thank's for used me!</h1>`;
 });
+//templating format in javascript;
 //the addEventListener() method is better than on click;
