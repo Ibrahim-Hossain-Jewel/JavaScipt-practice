ab48424dadad78f19a9845c80ad4e47f00efdc2d
[ibrahim hossain] <[ibrahimhossain495@gmail.com]>
2021-03-12 09:01:48 +0600

    addEventListener() with a message

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var cost = document.querySelector('input');
 var output = document.querySelector('h1');
 //these are has global access right now;
 //but the the var variable declaration is function scoped;
-
+output.innerText='The maximum fractional part is two';
 btn.addEventListener('click',()=>{
-    let holder = (cost.value*.121).toFixed(2);
+    let holder = (cost.value);
     //here let has bracket scope;
     output.innerHTML = holder;
 });
